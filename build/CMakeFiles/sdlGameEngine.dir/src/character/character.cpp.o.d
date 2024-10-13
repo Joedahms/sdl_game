46427586de656ad92e715293908f601ba1247521
Joedahms <dahms.joejoe@gmail.com>
2024-10-13 17:39:59 -0400

    Removed object files

--- build/CMakeFiles/sdlGameEngine.dir/src/character/character.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/sdlGameEngine.dir/src/character/character.cpp.o: \
- /home/joe/Documents/coding_projects/Trashore/src/character/character.cpp \
- /usr/include/stdc-predef.h \
- /home/joe/Documents/coding_projects/Trashore/src/character/character.h
