f9ffe26a4a00297d57ec170ad81640e291a2b6c8
Joedahms <dahms.joejoe@gmail.com>
2024-10-13 17:41:05 -0400

    added o to gitignore

---
+++ build/CMakeFiles/sdlGameEngine.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/joe/Documents/coding_projects/Trashore/src/button.cpp" "CMakeFiles/sdlGameEngine.dir/src/button.cpp.o" "gcc" "CMakeFiles/sdlGameEngine.dir/src/button.cpp.o.d"
+  "/home/joe/Documents/coding_projects/Trashore/src/camera/camera.cpp" "CMakeFiles/sdlGameEngine.dir/src/camera/camera.cpp.o" "gcc" "CMakeFiles/sdlGameEngine.dir/src/camera/camera.cpp.o.d"
+  "/home/joe/Documents/coding_projects/Trashore/src/character/character.cpp" "CMakeFiles/sdlGameEngine.dir/src/character/character.cpp.o" "gcc" "CMakeFiles/sdlGameEngine.dir/src/character/character.cpp.o.d"
+  "/home/joe/Documents/coding_projects/Trashore/src/character/character_factory.cpp" "CMakeFiles/sdlGameEngine.dir/src/character/character_factory.cpp.o" "gcc" "CMakeFiles/sdlGameEngine.dir/src/character/character_factory.cpp.o.d"
+  "/home/joe/Documents/coding_projects/Trashore/src/character/npc.cpp" "CMakeFiles/sdlGameEngine.dir/src/character/npc.cpp.o" "gcc" "CMakeFiles/sdlGameEngine.dir/src/character/npc.cpp.o.d"
+  "/home/joe/Documents/coding_projects/Trashore/src/character/player.cpp" "CMakeFiles/sdlGameEngine.dir/src/character/player.cpp.o" "gcc" "CMakeFiles/sdlGameEngine.dir/src/character/player.cpp.o.d"
+  "/home/joe/Documents/coding_projects/Trashore/src/game.cpp" "CMakeFiles/sdlGameEngine.dir/src/game.cpp.o" "gcc" "CMakeFiles/sdlGameEngine.dir/src/game.cpp.o.d"
+  "/home/joe/Documents/coding_projects/Trashore/src/gameplay.cpp" "CMakeFiles/sdlGameEngine.dir/src/gameplay.cpp.o" "gcc" "CMakeFiles/sdlGameEngine.dir/src/gameplay.cpp.o.d"
+  "/home/joe/Documents/coding_projects/Trashore/src/logger.cpp" "CMakeFiles/sdlGameEngine.dir/src/logger.cpp.o" "gcc" "CMakeFiles/sdlGameEngine.dir/src/logger.cpp.o.d"
+  "/home/joe/Documents/coding_projects/Trashore/src/main.cpp" "CMakeFiles/sdlGameEngine.dir/src/main.cpp.o" "gcc" "CMakeFiles/sdlGameEngine.dir/src/main.cpp.o.d"
+  "/home/joe/Documents/coding_projects/Trashore/src/main_menu.cpp" "CMakeFiles/sdlGameEngine.dir/src/main_menu.cpp.o" "gcc" "CMakeFiles/sdlGameEngine.dir/src/main_menu.cpp.o.d"
+  "/home/joe/Documents/coding_projects/Trashore/src/text.cpp" "CMakeFiles/sdlGameEngine.dir/src/text.cpp.o" "gcc" "CMakeFiles/sdlGameEngine.dir/src/text.cpp.o.d"
+  "/home/joe/Documents/coding_projects/Trashore/src/tile/dirt_tile.cpp" "CMakeFiles/sdlGameEngine.dir/src/tile/dirt_tile.cpp.o" "gcc" "CMakeFiles/sdlGameEngine.dir/src/tile/dirt_tile.cpp.o.d"
+  "/home/joe/Documents/coding_projects/Trashore/src/tile/tile.cpp" "CMakeFiles/sdlGameEngine.dir/src/tile/tile.cpp.o" "gcc" "CMakeFiles/sdlGameEngine.dir/src/tile/tile.cpp.o.d"
+  "/home/joe/Documents/coding_projects/Trashore/src/tile/tile_factory.cpp" "CMakeFiles/sdlGameEngine.dir/src/tile/tile_factory.cpp.o" "gcc" "CMakeFiles/sdlGameEngine.dir/src/tile/tile_factory.cpp.o.d"
+  "/home/joe/Documents/coding_projects/Trashore/src/tile/tile_map.cpp" "CMakeFiles/sdlGameEngine.dir/src/tile/tile_map.cpp.o" "gcc" "CMakeFiles/sdlGameEngine.dir/src/tile/tile_map.cpp.o.d"
+  "/home/joe/Documents/coding_projects/Trashore/src/tile/water_tile.cpp" "CMakeFiles/sdlGameEngine.dir/src/tile/water_tile.cpp.o" "gcc" "CMakeFiles/sdlGameEngine.dir/src/tile/water_tile.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
